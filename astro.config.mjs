56d1d3d38c42c6e8fc0427da10274ccd60548b0b
Alejandro Graziani <55697681+AleGraziani@users.noreply.github.com>
2025-09-02 15:36:40 -0300

    last test

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   redirects: {
-    "/tiendas": "/proyecto/index.html",
-    "/tiendas/": "/proyecto/index.html",
+    "/tiendas": "/tiendas/index.html",
+    "/tiendas/": "/tiendas/index.html",
   },
 })
